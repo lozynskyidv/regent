5da1829419802f3934e0d3c63d96f9c0389cf294
Dmytro Lozynskyi <lozynskyidv@gmail.com>
2026-01-06 12:36:46 +0000

    Fix: Wrap app in GestureHandlerRootView for swipe gestures
    Critical fix for swipe-to-edit/delete functionality.
    Without GestureHandlerRootView, Swipeable components throw error:
    'PanGestureHandler must be used as a descendant of GestureHandlerRootView'
    
    This is required setup for react-native-gesture-handler library.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,13 @@
 import { Slot } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { DataProvider } from '../contexts/DataContext';
 
 export default function RootLayout() {
   return (
-    <DataProvider>
-      <Slot />
-    </DataProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <DataProvider>
+        <Slot />
+      </DataProvider>
+    </GestureHandlerRootView>
   );
 }
